d9ff313fbe369c9a6af2f4350ac097a4816e394a
dennisquiros1 <114965553+dennisquiros1@users.noreply.github.com>
2025-01-26 18:59:59 -0600

    Change Inicial

---
+++ Proyecto_lenguajes/Models/Entities/Student.cs
@@ -0,0 +1,30 @@
+﻿namespace Proyecto_lenguajes.Models.Entities
+{
+    public class Student
+    {
+        private String id;
+        private String name;
+        private String lastName;
+        private String password;
+        private String email;
+
+        public Student()
+        {
+        }
+
+        public Student(string id, string name, string lastName, string password, string email)
+        {
+            Id = id;
+            Name = name;
+            LastName = lastName;
+            Password = password;
+            Email = email;
+        }
+
+        public string Id { get => id; set => id = value; }
+        public string Name { get => name; set => name = value; }
+        public string LastName { get => lastName; set => lastName = value; }
+        public string Password { get => password; set => password = value; }
+        public string Email { get => email; set => email = value; }
+    }
+}
